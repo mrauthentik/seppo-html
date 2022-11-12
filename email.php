c0240c54b28e3b691b9c36f50161cf99b0209962
Mr Authentik <umokeuchenna2020@gmail.com>
2022-11-12 18:52:12 +0100

    Deleted Unwanted files

---
+++ email.php
@@ -0,0 +1,11 @@
+<?php
+
+isset($_POST['email']){
+    $useremail = $_POST['your-email'];
+    $subject = $_POST['your-subject'];
+    $name = $_POST['your-name'];
+    $message = $_POST['your-message'];
+    $picture = $_POST['your-picture']
+    echo $useremail; 
+}
+?>
